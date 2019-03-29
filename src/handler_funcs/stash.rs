c7ad98bc1ce0bec88df5df22c827b1e885863c18
zwieberl <donko@posteo.de>
2019-03-29 11:08:56 +0100

    stash: Now there is a separate stash for each user

--- src/handler_funcs/stash.rs
+++ src/handler_funcs/stash.rs
@@ -1,6 +1,6 @@
 use matrix_bot_api::{MatrixBot, MessageType};
 use matrix_bot_api::handlers::{Message, MessageHandler, extract_command, HandleResult};
-
+use std::collections::HashMap;
 use crate::languages::*;
 use crate::tr;
 
@@ -33,13 +33,13 @@ pub fn help_str() -> String {
 // Our handler wants a mutable state (here represented by a little counter-variable)
 // This counter can be increased or decreased by users giving the bot a command.
 pub struct StashHandler {
-    stashes: Vec<String>,
+    stashes: HashMap<String, Vec<String>>,
     cmd_prefix: String,
 }
 
 impl StashHandler {
     pub fn new() -> StashHandler {
-        StashHandler{stashes: Vec::new(), cmd_prefix: "!".to_string()}
+        StashHandler{stashes: HashMap::new(), cmd_prefix: "!".to_string()}
     }
 
     /// With what prefix commands to the bot will start
@@ -48,41 +48,51 @@ impl StashHandler {
         self.cmd_prefix = prefix.to_string();
     }
 
-    fn list(&mut self) -> String {
+    fn list(&mut self, user: &str) -> String {
     	let mut message = String::new();
-    	if self.stashes.len() == 0 {
-			message += tr!("Stash is empty.");
-    	} else {
-	    	for (idx, stash) in &mut self.stashes.iter().enumerate() {
-	    	    message += &format!("[{}] - {:.25}\n", idx, &stash);
-	    	}
-    	}
+        if let Some(curr_stash) = self.stashes.get_mut(user) {
+        	if curr_stash.len() == 0 {
+    			message += tr!("Stash is empty.");
+        	} else {
+    	    	for (idx, stash) in &mut curr_stash.iter().enumerate() {
+    	    	    message += &format!("[{}] - {:.25}\n", idx, &stash);
+    	    	}
+        	}
+        } else {
+            message += tr!("Stash is empty.");
+        }
     	message
     }
 
-    pub fn del(&mut self, msg: &str)  -> String {
+    pub fn del(&mut self, user: &str, msg: &str)  -> String {
     	let idx: usize = match msg.parse() {
     	    Ok(x) => x,
     	    Err(_) => { return format!("\"{}\" {}.", msg, tr!("is not a number.")); },
     	};
 
     	let mut message = String::new();
-    	let length = self.stashes.len();
-    	if length == 0 {
-    		message += tr!("Stash is empty.");
-    	} else if idx < length {
-    		self.stashes.remove(idx);
-    		message += &format!("{} {}", tr!("Deleted index"), idx);
-    	} else {
-    		message += tr!("Index out of range!");
-    	}
+
+        if let Some(curr_stash) = self.stashes.get_mut(user) {
+            let length = curr_stash.len();
+            if length == 0 {
+                message += tr!("Stash is empty.");
+            } else if idx < length {
+                curr_stash.remove(idx);
+                message += &format!("{} {}", tr!("Deleted index"), idx);
+            } else {
+                message += tr!("Index out of range!");
+            }
+        } else {
+            message += tr!("Stash is empty.");
+        }
+
 
     	message
     }
 
-    pub fn show(&mut self, msg: &str)  -> String {
+    pub fn show(&mut self, user: &str, msg: &str)  -> String {
     	if msg.is_empty() {
-    		return self.list();
+    		return self.list(user);
     	}
 
     	let idx: usize = match msg.parse() {
@@ -91,21 +101,29 @@ impl StashHandler {
     	};
 
     	let mut message = String::new();
-    	let length = self.stashes.len();
-    	if length == 0 {
-    		message += tr!("Stash is empty.");
-    	} else if idx < length {
-    		message += &self.stashes[idx];
-    	} else {
-    		message += tr!("Index out of range!");
-    	}
+        if let Some(curr_stash) = self.stashes.get_mut(user) {
+            let length = curr_stash.len();
+            if length == 0 {
+                message += tr!("Stash is empty.");
+            } else if idx < length {
+                message += &curr_stash[idx];
+            } else {
+                message += tr!("Index out of range!");
+            }
+        } else {
+            message += tr!("Stash is empty.");
+        }
 
     	message
     }
 
-    pub fn stash(&mut self, msg: &str) -> String {
-    	self.stashes.push(msg.to_string());
-    	let length = self.stashes.len();
+    pub fn stash(&mut self, user: &str, msg: &str) -> String {
+        if !self.stashes.contains_key(user) {
+            self.stashes.insert(user.to_string(), Vec::new());
+        }
+        // we can unwrap here as we know that there has to be something inside
+    	self.stashes.get_mut(user).unwrap().push(msg.to_string());
+    	let length = self.stashes[user].len();
     	format!("{} {}", tr!("Added text at index"), length - 1)
     }
 }
@@ -115,6 +133,8 @@ impl StashHandler {
 // new (text-)message in the room the bot is in.
 impl MessageHandler for StashHandler {
     fn handle_message(&mut self, bot: &MatrixBot, message: &Message) -> HandleResult {
+        // Get who sent it
+        let user = &message.sender;
         // extract_command() will split the message by whitespace and remove the prefix (here "!")
         // from the first entry. If the message does not start with the given prefix, None is returned.
         let command = match extract_command(&message.body, &self.cmd_prefix) {
@@ -129,10 +149,10 @@ impl MessageHandler for StashHandler {
         // and a specific function for it (like StatelessHandler does it),
         // or you can use a simple match-statement, to act on the given command:
         let answer = match command {
-          x if x == tr!("list")  => self.list(),
-          x if x == tr!("stash") => self.stash(&message.body[end_of_prefix..].trim()),
-          x if x == tr!("show")  => self.show(&message.body[end_of_prefix..].trim()),
-          x if x == tr!("del")   => self.del(&message.body[end_of_prefix..].trim()),
+          x if x == tr!("list")  => self.list(user),
+          x if x == tr!("stash") => self.stash(user, &message.body[end_of_prefix..].trim()),
+          x if x == tr!("show")  => self.show(user, &message.body[end_of_prefix..].trim()),
+          x if x == tr!("del")   => self.del(user, &message.body[end_of_prefix..].trim()),
           _ => { return HandleResult::ContinueHandling; } /* Not a known command */
         };
 
